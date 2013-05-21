690f99d75dfebef16322a9885deca9de9d836b8b
Jessy Diamond Exum <diamondman@diamondman-laptop.(none)>
2013-05-21 01:03:24 -0700

    Finally got the method def I wanted working for the function I open gif files with.

--- src/main.c
+++ src/main.c
@@ -5,50 +5,44 @@
 #include <sys/stat.h>
 #include "gif.h"
 
-uint8_t* loadFile(char *path, uint32_t* data_copied){
+int loadFile(uint8_t **filebuffer, uint32_t* data_copied, char *path){
   struct stat buf;
   FILE *fp;
-  uint8_t *buffer;
 
-  if(stat(path, &buf) < 0){
+  if (stat(path, &buf) < 0){
     printf("FILE DOESN'T EXIST!\n");
-    *data_copied = -1;
-    return NULL;
+    return -1;
   }
-  if(!S_ISREG(buf.st_mode)){
+  if (!S_ISREG(buf.st_mode)){
    printf("Please Specify a file, not a directory/block device/etc.\n");
-   *data_copied = -2;
-   return NULL;
+   return -2;
   }
-  if((fp = fopen(path, "r")) == NULL){
+  if ((fp = fopen(path, "r")) == NULL){
     printf("Error Opening Image!\n");
-    *data_copied = -3;
-    return NULL;
+    return -3;
   }
-  if(buf.st_size > 0x80000000){
+  if (buf.st_size > 0x80000000){
     printf("Provided file too big! Max Size 2GB.\n");
-    *data_copied = -4;
-    return NULL;
+    return -4;
   }
 
-  buffer = (uint8_t*)malloc(buf.st_size);
-  *data_copied = fread(buffer, sizeof(char), buf.st_size, fp);
+  *filebuffer = (uint8_t*)malloc(buf.st_size);
+  *data_copied = fread(*filebuffer, sizeof(char), buf.st_size, fp);
 
   fclose(fp);
-  return buffer;
+  return 0;
 }
 
 int main(int argc, char* argv[]){
-  if(argc<2){ printf("Please Specify a gif Image to process.\n"); return 1; }
+  if (argc<2){ printf("Please Specify a gif Image to process.\n"); return -1; }
 
   Gif g;
   memset(&g, 0, sizeof(Gif));
+  uint8_t *filebuffer = NULL;
   int32_t data_copied = 0;
-  int8_t *source = loadFile(argv[1], &data_copied);
-  if(source==NULL) return -1;
 
-  int loadres = gif_load(&g, source, data_copied);
-  free(source);
-  if (loadres>=0) gif_printImageData(&g);
+  if (loadFile(&filebuffer, &data_copied, argv[1])<0) return -2;
+  if (gif_load(&g, filebuffer, data_copied)>=0) gif_printImageData(&g);
+  free(filebuffer);
   gif_free(&g);
 }
